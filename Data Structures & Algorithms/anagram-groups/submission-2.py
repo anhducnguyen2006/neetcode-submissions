211ebb1b483143cf80cb0a4bb40aaae7d9b72a36
Anh Duc Nguyen <108985445+anhducnguyen2006@users.noreply.github.com>
2026-04-19 15:20:03 -0400

    Update: anagram-groups - submission-2

--- Data Structures & Algorithms/anagram-groups/submission-2.py
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -1,20 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = {}
+        d = {}
 
         for i in strs: 
-            s = [0 for _ in range(26)]
-
-            for j in i: 
-                s[ord(j) - 97] += 1
-            
-            t = tuple(s)
-            if t not in dic: 
-                dic[t] = []
-                dic[t].append(i)
-            else:
-                dic[t].append(i)
+            t = tuple(sorted([x for x in i]))
+            if t not in d: 
+                d[t] = []
+            d[t].append(i)
         
-        return list(dic.values())
-            
+        ret = list(d.values())
+        return ret
     
